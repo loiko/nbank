531d6077a0b26718eeefaeb94cfdf1b7f03ddca0
loiko <mikalai.loika@gmail.com>
2026-01-31 00:29:31 +0200

    gut hun actions selenoid debug added v6

--- src/test/java/ui/utilities/BaseUiTest.java
+++ src/test/java/ui/utilities/BaseUiTest.java
@@ -20,8 +20,13 @@ public class BaseUiTest extends BaseTest {
 
     @BeforeAll
     public static void setupSelenoid() {
-        Configuration.remote = Config.getProperty("uiRemote");
-        Configuration.baseUrl = Config.getProperty("uiBaseUrl");
+
+        System.out.println("=== Configuration ===");
+        System.out.println("remote: " + Configuration.remote);
+        System.out.println("baseUrl: " + Configuration.baseUrl);
+        System.out.println("browser: " + Configuration.browser);
+//        Configuration.remote = Config.getProperty("uiRemote");
+//        Configuration.baseUrl = Config.getProperty("uiBaseUrl");
         Configuration.browser = Config.getProperty("browser");
         Configuration.browserSize = Config.getProperty("browserSize");
         Configuration.headless = true;
